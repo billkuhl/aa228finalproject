e57cfede2b123a27b700572624ce0082c82e18e5
billkuhl <46409096+billkuhl@users.noreply.github.com>
2023-11-08 14:09:34 -0800

    Create simplesim.jl

---
+++ simplesim.jl
@@ -0,0 +1,13 @@
+using Plots
+using LinearAlgebra
+using SatelliteToolbox
+
+orb = KeplerianElements(
+                        date_to_jd(2023,01,01),
+                        7190.982e3,
+                        0.001111,
+                        98.405 |> deg2rad,
+                        100    |> deg2rad,
+                        90     |> deg2rad,
+                        19     |> deg2rad
+                        )
