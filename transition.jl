f423db802e7a676e4a7ba156657608062b5df46a
billkuhl <46409096+billkuhl@users.noreply.github.com>
2023-12-03 14:35:39 -0800

    I thnk my side is good, but idk

--- transition.jl
+++ transition.jl
@@ -1,3 +1,5 @@
+include("structure.jl")
+
 function prop_to_S(orbp::SatelliteToolboxPropagators.OrbitPropagatorTwoBody,t)
 	# Turns propogators into state at a particular point.
 	# t is a specific timestamp in seconds relative to the initialized date (Jan 1 2023)
@@ -6,7 +8,7 @@ function prop_to_S(orbp::SatelliteToolboxPropagators.OrbitPropagatorTwoBody,t)
 	return new_sat
 end
 
-function gen_orbp(state)
+function gen_orbp(state::SatState)
 	
 	kep = rv_to_kepler(state.x,state.v)
 	prop = Propagators.init(Val(:TwoBody),kep)
@@ -14,23 +16,33 @@ function gen_orbp(state)
 	
 end
 
-function prop_state(state,dt)
+function prop_state(state::SatState,dt)
 	orbp = gen_orbp(state)
 	new_x,new_v = Propagators.propagate!(orbp,dt)
 	new_state = SatState(new_x,new_v)
 	return new_state
 end
 
-function next_state(state, a, dt::Int)
-	
+function next_state(state::MDPState, a)
+
+	dt = 10 #randomly chosen, can change later
 	unit_dV = 200.0 #m/s^2
     if a == 0
+        new_sat = state.sat
     else
         dV_mag = unit_dV*a
-        u_vel = vel/norm(vel)
-        s_changed_vel = SatState(state.sat.x, vel + dV_mag*u_vel)
-        new_orbp = gen_orbp()
-        return get_S(new_orbp, t0 + dt)
+        u_vel = state.sat.v/norm(state.sat.v)
+        sat_changed_vel = SatState(state.sat.x, vel + dV_mag*u_vel)
+        new_sat = prop_state(sat_changed_vel, dt)
     end
+
+    new_intruders = []
+    for intruder in state.intruders
+        next_pos = prop_state(intruder, dt)
+        new_intruders = vcat(new_intruders,next_pos)
+    end
+
+    new_MDPState = MDPState(new_sat,new_intruders)
+    return new_MDPState
 end 
 
