7ab2e785334d736d7a096316d676596cf0f2945c
kseefried <seefried@stanford.edu>
2023-12-04 13:33:58 -0800

    New version with generative function?

---
+++ mdp.jl
@@ -0,0 +1,24 @@
+using POMDPs
+using POMDPModels
+using MCTS
+using StaticArrays
+
+initial_state = 
+
+satellite = QuickMDP(
+    gen = function(s,a)
+        r = get_R(s,a)
+        sp = next_state(s,a)
+        return (sp = sp, r = r)
+    end, 
+    actions = [-1.,0,1],
+    discount = 0.95,
+    transition = function(s,a)
+        Deterministic(next_state(s,a))
+    end, 
+    initialstate = Deterministic(initial_state)
+
+)
+
+solver = MCTSSolver(n_iterations = 20, depth = 20, exploration_constant = 5.0)
+planner = solve(solver,satellite)
