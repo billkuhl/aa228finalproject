3cdf71c94b0b8dbdb31856d508818fb2c1f0b8e1
billkuhl <46409096+billkuhl@users.noreply.github.com>
2023-12-07 20:42:05 -0800

    multiple collisioin data in 2d and 3d added

--- mdp.jl
+++ mdp.jl
@@ -24,7 +24,7 @@ initial_kep_pos = KeplerianElements(
                                     date_to_jd(2023,01,01), # Epoch
                                     7190.982e3, # Semi-Major Axis
                                     0, # eccentricity
-                                    0 |> deg2rad, # Inclination
+                                    45 |> deg2rad, # Inclination
                                     0    |> deg2rad, # Right Angle of Ascending Node
                                     0     |> deg2rad, # Arg. of Perigree
                                     0     |> deg2rad # True Anomaly
@@ -35,15 +35,23 @@ x_initial, v_initial = kepler_to_rv(initial_kep_pos) # convert to position and v
 initial_sat_state = SatState(x_initial, v_initial) # create a SatState structure 
 
 # x_5k,v_5k = Propagators.propagate!(gen_orbp(initial_sat_state),10000) 
-set_impact_time = 1000
-x_5k,v_5k = Propagators.propagate!(gen_orbp(initial_sat_state),set_impact_time) 
-
 Random.seed!(123)
-intruder_v_noise = rand(MvNormal([0,0,0],Diagonal([500,500,0]))) # noise related to the intruder position
-intruder_collide_state = SatState(x_5k,v_5k+intruder_v_noise) 
-# intruder_initial_state = prop_state(intruder_collide_state,-10000) # creating a SatState structure with the noise for intruder
-intruder_initial_state = prop_state(intruder_collide_state,-set_impact_time)
-initial_state = MDPState(initial_sat_state,[intruder_initial_state]) # Creates the initial state for our MDP
+function create_impact_sat(to, init)
+    x_5k,v_5k = Propagators.propagate!(gen_orbp(init),to) 
+    
+    intruder_v_noise = rand(MvNormal([0,0,0],Diagonal([500,500,500]))) # noise related to the intruder position
+    intruder_collide_state = SatState(x_5k,v_5k+intruder_v_noise) 
+    # intruder_initial_state = prop_state(intruder_collide_state,-10000) # creating a SatState structure with the noise for intruder
+    intruder_initial_state = prop_state(intruder_collide_state,-to)
+    return intruder_initial_state
+end
+
+
+intruder_list = []
+for impact_time in [500,1000,5000,10000]
+    push!(intruder_list,create_impact_sat(impact_time,initial_sat_state))
+end
+initial_state = MDPState(initial_sat_state,intruder_list) # Creates the initial state for our MDP
 
 println("0. Initialize MDP")
 satellite = QuickMDP(
@@ -85,49 +93,52 @@ println("3. Generate Trajectory")
 states = []
 rewards = []
 actions = []
-for (s,a,r) in ProgressBar(stepthrough(satellite,policy,"s,a,r", max_steps=100))
+for (s,a,r) in ProgressBar(stepthrough(satellite,policy,"s,a,r", max_steps=150))
     append!(states,[s])
 	append!(actions,[a])
 	append!(rewards,[r])
 end
 
-intruder_x = []
+intruder_x = [[] for i in range(1,length(intruder_list))]
+
 target_x = []
 for s in states
     push!(target_x,s.sat.x)
-    for intruder in s.intruders
-        push!(intruder_x,intruder.x)
+    for (i,intruder) in enumerate(s.intruders)
+
+        push!(intruder_x[i], intruder.x)
+
     end
 end
 
-
+println(intruder_x)
 println("4. Store Data")
 
 data = Dict("sat"=>target_x, "intruder"=>intruder_x, "actions" => actions, "rewards" => rewards)
 json_string = JSON.json(data)
 
-open("data\\2d_1k.json","w") do f
+open("data\\3d_5s1k5k10k.json","w") do f
   JSON.print(f, json_string)
 end
 
-println("5. Evaluate Policies")
-solver1 = MCTSSolver(n_iterations = 50000, depth = 10, exploration_constant = 5.0, enable_tree_vis=true)
-policy1 = solve(solver1, satellite) 
-u1 = evaluate(satellite,policy1)
-value1 = u1(initial_state)
-println("Value 1: $value1")
+# println("5. Evaluate Policies")
+# solver1 = MCTSSolver(n_iterations = 50000, depth = 10, exploration_constant = 5.0, enable_tree_vis=true)
+# policy1 = solve(solver1, satellite) 
+# u1 = evaluate(satellite,policy1)
+# value1 = u1(initial_state)
+# println("Value 1: $value1")
 
-solver2 = MCTSSolver(n_iterations = 50000, depth = 20, exploration_constant = 5.0, enable_tree_vis=true)
-policy2 = solve(solver2, satellite)
-u2 = evaluate(satellite,policy2)
-value2 = u2(initial_state)
-println("Value 2: $value2")
+# solver2 = MCTSSolver(n_iterations = 50000, depth = 20, exploration_constant = 5.0, enable_tree_vis=true)
+# policy2 = solve(solver2, satellite)
+# u2 = evaluate(satellite,policy2)
+# value2 = u2(initial_state)
+# println("Value 2: $value2")
 
-solver3 = MCTSSolver(n_iterations = 50000, depth = 10, exploration_constant = 10.0, enable_tree_vis=true)
-policy3 = solve(solver3, satellite)
-u3 = evaluate(satellite,policy3)
-value3 = u3(initial_state)
-println("Value 3: $value3")
+# solver3 = MCTSSolver(n_iterations = 50000, depth = 10, exploration_constant = 10.0, enable_tree_vis=true)
+# policy3 = solve(solver3, satellite)
+# u3 = evaluate(satellite,policy3)
+# value3 = u3(initial_state)
+# println("Value 3: $value3")
 
 # a,info = action_info(policy,initial_state)
 # tree = info[:tree]
